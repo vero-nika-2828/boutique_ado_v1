bed9d71361b75006db2bf7f6a868fb1721b9818a
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-09-28 21:56:48 +0000

    Start product template

--- products/models.py
+++ products/models.py
@@ -8,7 +8,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True)
 
     class Meta:
         """
